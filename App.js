ccd6efdd8d153d71f7e8ea45fe3cc74fbee8ca70
Trimaanpreet <149078286+Trimaanpreet@users.noreply.github.com>
2025-12-14 19:45:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Offers from "./Offers";
+import Sweets from "./Sweets";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Offers />
+      <Sweets />
+    </>
+  );
+}
+export default App;
